d4b97a189610d016edce1a4502a7294aa03ba21d
Rupsa Adhikary <165818067+Rupsa004@users.noreply.github.com>
2024-07-21 17:16:35 +0000

    variable

---
+++ Basics/variable.js
@@ -0,0 +1,17 @@
+const accId= 224433
+let accEmail= "kaushikir766@gmail.com" // 2 mthods used for declaring variables : let & var
+var accPassword = "653261"
+accCity= "Kolkata"
+let accStatus; //undefined
+
+//accId=112233
+//console.log(accId)
+
+/* prefer not to use var
+bacause of issue in block scope and func scope in js (scope:{}) */
+
+accEmail="rai004@gmail.com"
+accPassword="643287"
+accCity="Durgapur"
+
+console.table([accId, accEmail, accPassword, accCity, accStatus])
